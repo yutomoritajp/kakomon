4cffb0e9dbcca4dbafc529cb889d156f5137ca04
yuto <teg.moritayuto@gmail.com>
2026-06-09 21:40:03 +0900

    v.0.1 出題画面作成

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,7 +1,11 @@
 
 const Quiz = () => {
     return (
-      <>Quizページ</>
+      <>
+        <h2 className='text-2xl font-yuji underline'>データベーススペシャリスト試験過去問</h2>
+        <h3 className='text-xl font-jp font-bold mt-8'>第一問</h3>
+        <p className='text-lg font-jp mt-4'>導出表に関する記述として適切なものはどれか。</p>
+      </>
     )
 }
 
